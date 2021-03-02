56044153433b8baa57141c3e8917294e2322913c
et3535 <vet3535v@gmail.com>
2021-03-03 00:14:03 +0900

    first commit

---
+++ src/main/java/com/example/demo/Pjt01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pjt01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pjt01Application.class, args);
+	}
+
+}
